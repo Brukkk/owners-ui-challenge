4461bd80f6939b6e65bbdb23fa050ba6c19ba521
Bruno Torrabadella <bruktorra@gmail.com>
2023-09-26 14:14:25 -0300

    Inter font

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -3,6 +3,9 @@ import { createTheme } from "@mui/material/styles";
 const rootElement = document.getElementById("root");
 // Change the target container for Portal-related elements so that they are injected under the main app wrapper.
 export const theme = createTheme({
+  typography: {
+    fontFamily: ["Inter", "sans-serif"].join(","),
+  },
   components: {
     MuiPopover: {
       defaultProps: {
