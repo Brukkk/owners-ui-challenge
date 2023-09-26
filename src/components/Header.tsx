58725cca2fa23194a2ebcc1e52b8811710bdcc05
Bruno Torrabadella <bruktorra@gmail.com>
2023-09-26 03:14:36 -0300

    Fix Header small aligment with badge due to icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Badge from "@mui/material/Badge";
 
 export const Header = () => {
   return (
-    <header className="sticky left-0 right-0 top-0 flex justify-between bg-white p-7 text-[#304985]">
+    <header className=" sticky left-0 right-0 top-0 z-10 flex justify-between bg-white p-7 text-[#304985]">
       <MenuIcon className=" cursor-pointer" />
       <div className="flex max-h-6 gap-6">
         <Badge
@@ -15,7 +15,7 @@ export const Header = () => {
           slotProps={{
             badge: {
               className:
-                "h-3 w-3 bottom-1.5 right-1.5 min-w-0  bg-[#BD2020] p-0.5 text-[8px] leading-tight text-white",
+                "h-3 w-3 bottom-2 right-1 min-w-0 content-between  bg-[#BD2020] p-0.5 text-[8px] leading-tight text-white",
             },
           }}
         >
@@ -28,7 +28,7 @@ export const Header = () => {
           slotProps={{
             badge: {
               className:
-                "h-3 w-3 bottom-1.5 right-1.5 min-w-0  bg-[#BD2020] p-0.5 text-[8px] leading-tight text-white",
+                "h-3 w-3 bottom-1.5 right-1.5 min-w-0 content-between  bg-[#BD2020] p-0.5 text-[8px] leading-tight text-white",
             },
           }}
         >
